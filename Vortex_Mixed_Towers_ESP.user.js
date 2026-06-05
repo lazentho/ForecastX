cf7e1abeeec4c6ef9d742f87909d01243d5d860a
lazentho <t.vd.leije@gmail.com>
2026-06-05 15:36:34 +0200

    Add Vortex mixed Towers ESP userscript

---
+++ Vortex_Mixed_Towers_ESP.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Vortex Client - Full Menu + Towers Random ESP
+// @namespace    http://tampermonkey.net/
+// @version      9.0
+// @description  Full Vortex overlay with persistent themes, Slide visual highlight, wallet sync, and Towers random visual ESP.
+// @author       Vortex
+// @match        https://bloxflip.com/*
+// @match        https://*.bloxflip.com/*
+// @match        https://bloxflip.net/*
+// @match        https://*.blox
